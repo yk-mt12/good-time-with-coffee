5ab44daa48283645219a9464c2681a007b5828b7
yk-mt12 <agvksl543@gmail.com>
2022-03-13 09:13:05 +0900

    fix header and footer

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -3,7 +3,7 @@ import React, { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
   return (
-    <Box  style={{backgroundColor:"#344955", color:"#fff", height: "80px", paddingTop: "30px"}}>
+    <Box  style={{backgroundColor:"#344955", color:"#fff", height: "50px", paddingTop: "5px", fontSize: "14px", fontWeight: "solid"}}>
       <h1>Good Time with Coffee</h1>
     </Box>
   );
