fe47a7ac03d8c6ef43947eef777fabb0a7b325c6
yk-mt12 <agvksl543@gmail.com>
2022-03-13 18:18:44 +0900

    add ratio

--- src/components/ui/ItemCard.tsx
+++ src/components/ui/ItemCard.tsx
@@ -8,8 +8,8 @@ export const ItemCard: React.VFC<Props> = React.memo((props)=> {
   const { beans } = props;
   return (
     <div style={{fontSize: "18px"}}>
-      <p>coffee ratio = 1:16</p>
       <p>Beans: <span style={{color: "primary"}}>{beans}</span></p>
+      <p style={{fontSize: "14px", color: "#fff"}}>Coffee ratio = 1:16</p>
     </div>
 
   );
